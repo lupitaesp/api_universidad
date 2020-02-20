7712f936ffb038d90ddacfcab3689de5d139e6c5
lupitaesp <1718110388@utectulancingo.edu.mx>
2020-02-20 10:52:19 -0600

    cambios varios3

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -15,14 +15,14 @@ class Alumnos:
             datos=web.input()     #Los datos introducidos por el usuario se almacenaran en datos
             if datos['token']=="1234":     #Si el usuario ingresa bien el token se declarara lo siguiente
                 result=[]           #Un arreglo
-                result2={}          #Un diccionario
+                result={}          #Un diccionario
                 if datos['action']=="get":        #Si accion es get va a hacer lo siguiente
                     with open('static/csv/alumnos.csv','r') as csvfile:   #Ruta del archivo csv que va a leer, r es de lectura, csvfile es una variable cualquiera
                         reader = csv.DictReader(csvfile)         #Lector del archivo, DictReader te almacena los datos como en diccionario en este caso en la variable reader
                         for row in reader:              #Lee la primer fila y la manda la arreglo
                             result.append(row)          #Lo manda al arreglo result
-                            result2['alumnos']=result      #Result2 en la posicion alumnos, sera lo que va a almacenar en result
-                    return json.dumps(result2)          #Va a regresar un json del result2 que es lo que va almacenando el arreglo
+                            result['alumnos']=result      #Result2 en la posicion alumnos, sera lo que va a almacenar en result
+                    return json.dumps(result)          #Va a regresar un json del result2 que es lo que va almacenando el arreglo
                 else:                           #Si accion no es get va a poner comando no encontrado
                     result2={}
                     result2['status']="Command not found"
